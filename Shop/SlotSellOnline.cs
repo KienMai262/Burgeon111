482ebe52c9948b4a5531f1438c6e48c3c946e7dd
KienMai262 <dihoc071104@gmail.com>
2024-11-09 20:04:30 +0700

    Xoa

--- Shop/SlotSellOnline.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SlotSellOnline : MonoBehaviour
-{
-    public ItemData code;
-    public int quantity;
-}
